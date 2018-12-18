3177dc2e16ffdd492a29d2c7164c549198729b7f
QuayAu <quayau@gmail.com>
2018-12-18 15:11:59 +0100

    update automated testing twoclass and multiclass

--- tests/testthat/test_classif_all.R
+++ tests/testthat/test_classif_all.R
@@ -1,52 +1,73 @@
 context("classif learners")
 
 test_that("learner construction", {
-  classif.learners = grep("classif.", mlr_learners$keys(), value = TRUE)
-  classif.learners = setdiff(classif.learners, "classif.debug")
-  learners = setNames(lapply(classif.learners, mlr_learners$get), classif.learners)
-  lapply(learners, expect_learner)
+  learners_string = grep("classif.", mlr_learners$keys(), value = TRUE)
+  learners_string = setdiff(learners_string, "classif.debug")
+  learners = characterToLearner(learners_string)
+
+  for (id in learners_string) expect_learner(learners[[id]])
 })
 
-test_that("simple train/predict", {
-  classif.learners = grep("classif.", mlr_learners$keys(), value = TRUE)
-  classif.learners = setdiff(classif.learners, "classif.debug")
-  learners = setNames(lapply(classif.learners, mlr_learners$get), classif.learners)
+testTrainPredict = function(id, task, learners) {
+  e = Experiment$new(task, learners[[id]])
+  e$train()
+  expect_false(e$has_errors, info = id)
+  e$predict()
+  expect_class(e$prediction, "Prediction", info = id)
+  e$score()
+  expect_number(e$performance, info = id)
+}
+
+test_that("simple train/predict multiclass", {
+  learners_string = grep("classif.", mlr_learners$keys(), value = TRUE)
+  learners_string = setdiff(learners_string, "classif.debug")
+  learners_string = filterLearnerByProperties(learners_string, "multiclass")
+  learners = characterToLearner(learners_string)
 
   task = mlr_tasks$get("iris")
-  experiments = lapply(learners, function(x) Experiment$new(task, x))
-  lapply(experiments, function(x) x$train())
-  lapply(experiments, function(x) expect_false(x$has_errors))
-  # FIXME: test models like expect_class(e$model, "ranger")
-  lapply(experiments, function(x) x$predict())
-  lapply(experiments, function(x) expect_class(x$prediction, "Prediction"))
-  lapply(experiments, function(x) x$score())
-  lapply(experiments, function(x) expect_number(x$performance))
+  for (id in learners_string) {
+    testTrainPredict(id, task, learners)
+  }
 })
 
-test_that("simple train/predict with probs", {
-  classif.learners = grep("classif.", mlr_learners$keys(), value = TRUE)
-  classif.learners = setdiff(classif.learners, "classif.debug")
-  learners = setNames(lapply(classif.learners, mlr_learners$get), classif.learners)
+test_that("simple train/predict twoclass", {
+  learners_string = grep("classif.", mlr_learners$keys(), value = TRUE)
+  learners_string = setdiff(learners_string, "classif.debug")
+  learners_string = filterLearnerByProperties(learners_string, "twoclass")
+  learners = characterToLearner(learners_string)
 
-  pt = lapply(learners, function(x) x$predict_types)
-  p_learners = setNames(lapply(classif.learners, mlr_learners$get), classif.learners)
-  p_learners = p_learners[unlist(lapply(pt, function(x) "prob" %in% x))]
+  task = mlr_tasks$get("sonar")
+  for (id in learners_string) {
+    testTrainPredict(id, task, learners)
+  }
+})
 
-  lapply(p_learners, function(x) expect_equal(x$predict_type, "response"))
+test_that("simple train/predict with probs multiclass", {
+  learners_string = grep("classif.", mlr_learners$keys(), value = TRUE)
+  learners_string = setdiff(learners_string, "classif.debug")
+  learners_string = filterLearnerByProperties(learners_string, "multiclass")
+  learners_string = filterLearnerByPredicttype(learners_string, "prob")
+  learners = characterToLearner(learners_string)
+  lapply(learners, function(x) x$predict_type = "prob")
 
   task = mlr_tasks$get("iris")
-  experiments = lapply(p_learners, function(x) Experiment$new(task, x))
+  for (id in learners_string) {
+    testTrainPredict(id, task, learners)
+  }
+})
 
-  lapply(p_learners, function(x) x$predict_type = "prob")
-  lapply(p_learners, function(x) expect_equal(x$predict_type, "prob"))
+test_that("simple train/predict with probs twoclass", {
+  learners_string = grep("classif.", mlr_learners$keys(), value = TRUE)
+  learners_string = setdiff(learners_string, "classif.debug")
+  learners_string = filterLearnerByProperties(learners_string, "twoclass")
+  learners_string = filterLearnerByPredicttype(learners_string, "prob")
+  learners = characterToLearner(learners_string)
+  lapply(learners, function(x) x$predict_type = "prob")
 
-  lapply(experiments, function(x) x$train())
-  lapply(experiments, function(x) expect_false(x$has_errors))
-  # FIXME: test models like expect_class(e$model, "ranger")
-  lapply(experiments, function(x) x$predict())
-  lapply(experiments, function(x) expect_class(x$prediction, "Prediction"))
-  lapply(experiments, function(x) x$score())
-  lapply(experiments, function(x) expect_number(x$performance))
+  task = mlr_tasks$get("sonar")
+  for (id in learners_string) {
+    testTrainPredict(id, task, learners)
+  }
 })
 
 test_that("extract importance", {
