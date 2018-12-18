7c846224a32ee5a29cceee08ab6d6ea80e6cfee7
QuayAu <quayau@gmail.com>
2018-12-18 15:15:09 +0100

    style guide update

--- R/helpers.R
+++ R/helpers.R
@@ -1,9 +1,8 @@
-propVectorToMatrix = function (p, levs)
-{
-    checkmate::assert_numeric(p)
-    y = matrix(0, ncol = 2L, nrow = length(p))
-    colnames(y) = levs
-    y[, 1L] = p
-    y[, 2L] = 1 - p
-    y
+propVectorToMatrix = function(p, levs) {
+  checkmate::assert_numeric(p)
+  y = matrix(0, ncol = 2L, nrow = length(p))
+  colnames(y) = levs
+  y[, 1L] = p
+  y[, 2L] = 1 - p
+  y
 }
