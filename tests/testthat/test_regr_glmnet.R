aba15626a06de882c6b5674b04cf419c4e4d15ca
Michel Lang <michellang@gmail.com>
2019-01-21 15:39:17 +0100

    fix glmnet tests

--- tests/testthat/test_regr_glmnet.R
+++ tests/testthat/test_regr_glmnet.R
@@ -1,6 +1,6 @@
 context("regr.glmnet")
 
 test_that("regr.glmnet test feature types", {
-  lrn = LearnerRegrGlmnet$new()
-  expect_autotest(lrn)
+  learner = LearnerClassifGlmnet$new()
+  expect_autotest(learner, exclude = "(feat_single|sanity)")
 })
