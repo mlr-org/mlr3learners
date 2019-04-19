105083b0de1b8ac6b96b6ebee16f4b638d946baa
Marvin Böcker <marvin.boecker@tu-dortmund.de>
2019-04-19 16:55:11 +0200

    finalize svm classif

--- R/mlr_learners_classif_svm.R
+++ R/mlr_learners_classif_svm.R
@@ -14,24 +14,24 @@ LearnerClassifSvm = R6Class("LearnerClassifSvm", inherit = LearnerClassif,
         id = id,
         param_set = ParamSet$new(
           params = list(
-            ParamFct$new(id = "type", default = "C-classification", levels = c("C-classification", "nu-classification")),
-            ParamDbl$new(id = "cost",  default = 1, lower = 0), #requires = quote(type == "C-classification")),
-            ParamDbl$new(id = "nu", default = 0.5), #requires = quote(type == "nu-classification")),
-            ParamFct$new(id = "kernel", default = "radial", levels = c("linear", "polynomial", "radial", "sigmoid")),
-            ParamInt$new(id = "degree", default = 3L, lower = 1L), #requires = quote(kernel == "polynomial")),
-            ParamDbl$new(id = "coef0", default = 0), #requires = quote(kernel == "polynomial" || kernel == "sigmoid")),
-            ParamDbl$new(id = "gamma", lower = 0), #requires = quote(kernel != "linear")),
-            ParamDbl$new(id = "cachesize", default = 40L),
-            ParamDbl$new(id = "tolerance", default = 0.001, lower = 0),
-            ParamLgl$new(id = "shrinking", default = TRUE),
-            ParamInt$new(id = "cross", default = 0L, lower = 0L), #tunable = FALSE),
-            ParamLgl$new(id = "fitted", default = TRUE), #tunable = FALSE),
-            ParamUty$new(id = "scale", default = TRUE)#, tunable = TRUE) # <- gehen so vektorparameter ?
+            ParamFct$new(id = "type", default = "C-classification", levels = c("C-classification", "nu-classification"), tag = "train"),
+            ParamDbl$new(id = "cost",  default = 1, lower = 0, tag = "train"), #requires = quote(type == "C-classification")),
+            ParamDbl$new(id = "nu", default = 0.5, tag = "train"), #requires = quote(type == "nu-classification")),
+            ParamFct$new(id = "kernel", default = "radial", levels = c("linear", "polynomial", "radial", "sigmoid"), tag = "train"),
+            ParamInt$new(id = "degree", default = 3L, lower = 1L, tag = "train"), #requires = quote(kernel == "polynomial")),
+            ParamDbl$new(id = "coef0", default = 0, tag = "train"), #requires = quote(kernel == "polynomial" || kernel == "sigmoid")),
+            ParamDbl$new(id = "gamma", lower = 0, tag = "train"), #requires = quote(kernel != "linear")),
+            ParamDbl$new(id = "cachesize", default = 40L, tag = "train"),
+            ParamDbl$new(id = "tolerance", default = 0.001, lower = 0, tag = "train"),
+            ParamLgl$new(id = "shrinking", default = TRUE, tag = "train"),
+            ParamInt$new(id = "cross", default = 0L, lower = 0L, tag = "train"), #tunable = FALSE),
+            ParamLgl$new(id = "fitted", default = TRUE, tag = "train"), #tunable = FALSE),
+            ParamUty$new(id = "scale", default = TRUE, tag = "train")#, tunable = TRUE)
           )
         ),
         predict_types = c("response", "prob"),
         feature_types = c("integer", "numeric"),
-        properties = c("twoclass", "multiclass", "weights"),
+        properties = c("twoclass", "multiclass"),
         packages = "e1071"
       )
     },
@@ -41,25 +41,12 @@ LearnerClassifSvm = R6Class("LearnerClassifSvm", inherit = LearnerClassif,
       data = as.matrix(task$data(cols = task$feature_names))
       target = as.factor(as.matrix(task$data(cols = task$target_names)))
 
-      if("weights" %in% task$properties) {
-        levs = as.character(task$class_names)
-        names(task$weights$weight) = levs
-
-        self$model = invoke(e1071::svm,
-          x = data,
-          y = target,
-          probability = self$predict_type == "prob",
-          class.weights = task$weights$weight,
-          .args = pars
-        )
-      } else {
-        self$model = invoke(e1071::svm,
-          x = data,
-          y = target,
-          probability = self$predict_type == "prob",
-          .args = pars
-        )
-      }
+      self$model = invoke(e1071::svm,
+        x = data,
+        y = target,
+        probability = self$predict_type == "prob",
+        .args = pars
+      )
 
       self
     },
@@ -67,15 +54,14 @@ LearnerClassifSvm = R6Class("LearnerClassifSvm", inherit = LearnerClassif,
     predict = function(task) {
       pars = self$params("predict")
       newdata = as.matrix(task$data(cols = task$feature_names))
-      response = prob = NULL
       levs = as.character(task$class_names)
 
-      probs = invoke(predict, self$model, newdata = newdata, probability = self$predict_type == "prob", .args = pars)
+      response = invoke(predict, self$model, newdata = newdata, probability = self$predict_type == "prob", .args = pars)
 
       if (self$predict_type == "prob") {
-        prob = attr(probs, "probabilities")
+        prob = attr(response, "probabilities")
       } else {
-        response = probs
+        prob = NULL
       }
 
       PredictionClassif$new(task, response, prob)
