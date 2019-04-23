1f8cbc7bb724836f3030f333164f98e104f8a21e
Michel Lang <michellang@gmail.com>
2019-04-23 15:03:51 +0200

    enable sanity check for regr.glmnet

--- tests/testthat/test_regr_glmnet.R
+++ tests/testthat/test_regr_glmnet.R
@@ -3,6 +3,6 @@ context("regr.glmnet")
 test_that("autotest", {
   learner = LearnerRegrGlmnet$new()
   expect_learner(learner)
-  result = run_autotest(learner, exclude = "(feat_single|sanity)")
+  result = run_autotest(learner, exclude = "feat_single")
   expect_true(result, info = result$error)
 })
