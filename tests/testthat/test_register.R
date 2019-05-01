0ed16978f001d53d0f5b4cca63f4e58c6cd16b47
Michel Lang <michellang@gmail.com>
2019-05-01 23:10:03 +0200

    fix test

--- tests/testthat/test_register.R
+++ tests/testthat/test_register.R
@@ -3,6 +3,6 @@ context("populate dictionaries")
 test_that("re-populate dictionaries", {
   rm("classif.ranger", envir = mlr_learners$items)
   expect_disjunct("classif.ranger", mlr_learners$keys())
-  populate_dictionaries()
+  register_mlr3()
   expect_subset("classif.ranger", mlr_learners$keys())
 })
