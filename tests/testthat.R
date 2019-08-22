0cadbe92f96f5cbcda56d635184fdfb1213cce66
Michel Lang <michellang@gmail.com>
2019-08-22 22:00:45 +0200

    make testthat optional

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,5 @@
-library(testthat)
-library(mlr3learners)
-
-test_check("mlr3learners")
+if (requireNamespace("testthat", quietly = TRUE)) {
+  library(testthat)
+  library(mlr3learners)
+  test_check("mlr3learners")
+}
