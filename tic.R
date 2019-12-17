a69921d8953e6ba9471a88ffeda21dc753a3cd6c
pat-s <patrick.schratz@gmail.com>
2019-12-17 13:29:29 +0100

    proper tic deployment

--- tic.R
+++ tic.R
@@ -1,7 +1,9 @@
 do_package_checks(error_on = "warning")
 
 if (ci_has_env("BUILD_PKGDOWN")) {
-  do_pkgdown(orphan = TRUE, install = TRUE)
+  get_stage("install") %>%
+    add_step(step_install_github("mlr-org/mlr3pkgdowntemplate"))
+  do_pkgdown()
 }
 
 get_stage("after_success") %>%
