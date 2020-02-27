368cec236e31f0f5d35999e1db88b351a72e2874
be-marc <marcbecker@posteo.de>
2020-02-27 20:25:30 +0100

    Add nnet::multinom

---
+++ R/LearnerClassifMultinom.R
@@ -0,0 +1,83 @@
+#' @title Multinomial log-linear learner via neural networks
+#'
+#' @name mlr_learners_classif.multinom
+#'
+#' @description
+#' Multinomial log-linear models via neural networks.
+#' Calls [nnet::multinom()] from package \CRANpkg{nnet}.
+#'
+#' @templateVar id classif.multinom
+#' @template section_dictionary_learner
+#'
+#' @export
+#' @template seealso_learner
+#' @template example
+LearnerClassifMultinom = R6Class("LearnerClassifMultinom", inherit = LearnerClassif,
+  public = list(
+
+    #' @description
+    #' Creates a new instance of this [R6][R6::R6Class] class.
+    initialize = function() {
+      ps = ParamSet$new(list(
+        ParamLgl$new("Hess", default = FALSE, tags = "train"),
+        ParamFct$new("summ", default = "0", levels = c("0", "1", "2", "3"), tags = "train"),
+        ParamLgl$new("censored", default = FALSE, tags = "train"),
+        ParamLgl$new("model", default = FALSE, tags = "train"),
+        ParamDbl$new("rang", default = 0.7, tags = "train"),
+        ParamDbl$new("decay", default = 0, tags = "train"),
+        ParamInt$new("maxit", default = 100L, lower = 1L, tags = "train"),
+        ParamLgl$new("trace", default = TRUE, tags = "train"),
+        ParamDbl$new("abstol", default = 1.0e-4, tags = "train"),
+        ParamDbl$new("reltol", default = 1.0e-8, tags = "train")
+      ))
+
+      super$initialize(
+        id = "classif.multinom",
+        param_set = ps,
+        predict_types = c("response", "prob"),
+        feature_types = c("logical", "integer", "numeric", "factor"),
+        properties = c("weights", "twoclass", "multiclass"),
+        packages = "nnet",
+        man = "mlr3learners::mlr_learners_classif.multinom"
+      )
+    }
+  ),
+
+  private = list(
+    .train = function(task) {
+      pars = self$param_set$get_values(tags = "train")
+      data = task$data()
+
+      if ("weights" %in% task$properties) {
+        pars$weights = task$weights$weight
+      }
+      if(!is.null(pars$summ)) {
+         pars$summ = as.integer(pars$summ)
+      }
+
+      if ("twoclass" %in% task$properties) {
+        data[[task$target_names]] = factor(data[[task$target_names]],
+          levels = c(task$positive, task$negative))
+      }
+
+      invoke(nnet::multinom, data = data, .args = pars)
+    },
+
+    .predict = function(task) {
+      newdata = task$data(cols = task$feature_names)
+      levs = task$class_names
+
+      if (self$predict_type == "response") {
+        response = invoke(predict, self$model, newdata = newdata, type = "class")
+        PredictionClassif$new(task = task, response = drop(response))
+      } else {
+        prob = invoke(predict, self$model, newdata = newdata, type = "probs")
+        if (length(levs) == 2L) {
+          prob = matrix(c(1 - prob, prob), ncol = 2L, byrow = FALSE)
+          colnames(prob) = levs
+        }
+        PredictionClassif$new(task = task, prob = prob)
+      }
+    }
+  )
+)
---
+++ tests/testthat/test_classif_multinom.R
@@ -0,0 +1,12 @@
+context("classif.multinom")
+
+skip_if_not_installed("nnet")
+
+test_that("autotest", {
+  learner = mlr3::lrn("classif.multinom")
+  expect_learner(learner)
+
+  skip_on_os("solaris")
+  result = run_autotest(learner)
+  expect_true(result, info = result$error)
+})
