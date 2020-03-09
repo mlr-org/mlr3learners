34177a217f21c8de658f889a62834271c150bd56
Michel Lang <michellang@gmail.com>
2020-03-09 14:45:01 +0100

    fix examples

--- man-roxygen/example.R
+++ man-roxygen/example.R
@@ -2,7 +2,7 @@
 lrn = mlr3::lrn(id)
 %>
 #' @examples
-#' if (requireNamespace("<%= lrn$packages %>") {
+#' if (requireNamespace("<%= lrn$packages %>")) {
 #'   learner = mlr3::lrn("<%= id %>")
 #'   print(learner)
 #'
