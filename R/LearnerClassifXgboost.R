0e3a1093ef6285726edd67f4540f77cc85ba87c8
Jakob Richter <code@jakob-r.de>
2020-03-26 17:32:57 +0100

    xgboost: remove wrong nrounds default

--- R/LearnerClassifXgboost.R
+++ R/LearnerClassifXgboost.R
@@ -90,7 +90,7 @@ LearnerClassifXgboost = R6Class("LearnerClassifXgboost", inherit = LearnerClassi
       ps$add_dep("top_k", "feature_selector", CondAnyOf$new(c("greedy", "thrifty")))
 
       # custom defaults
-      ps$values = list(nrounds = 1L, verbose = 0L)
+      ps$values = list(verbose = 0L)
 
       super$initialize(
         id = "classif.xgboost",
