4577f5af55286f4f6d41ea303b8d3a3ab3f39a2f
pat-s <patrick.schratz@gmail.com>
2020-04-14 17:38:11 +0200

    - Add a test that all learners can be constructed without parameters, fixes #78

---
+++ tests/testthat/test-test_construction.R
@@ -0,0 +1,22 @@
+test_that("all learners can be constructed without custom args", {
+  expect_silent(lrns(c(
+    "classif.glmnet",
+    "classif.kknn",
+    "classif.lda",
+    "classif.log_reg",
+    "classif.naive_bayes",
+    "classif.qda",
+    "classif.ranger",
+    "classif.svm",
+    "classif.xgboost",
+    "classif.multinom",
+
+    "regr.glmnet",
+    "regr.kknn",
+    "regr.km",
+    "regr.lm",
+    "regr.ranger",
+    "regr.svm",
+    "regr.xgboost"))
+  )
+})
