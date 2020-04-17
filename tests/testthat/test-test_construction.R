0921e1f37ab24d3c88043b9df140eff81e42ae63
pat-s <patrick.schratz@gmail.com>
2020-04-17 15:24:53 +0200

    update test-construction

--- tests/testthat/test-test_construction.R
+++ tests/testthat/test-test_construction.R
@@ -1,6 +1,7 @@
 test_that("all learners can be constructed without custom args", {
   expect_silent(lrns(c(
     "classif.cv_glmnet",
+    "classif.glmnet",
     "classif.kknn",
     "classif.lda",
     "classif.log_reg",
@@ -12,6 +13,7 @@ test_that("all learners can be constructed without custom args", {
     "classif.multinom",
 
     "regr.cv_glmnet",
+    "regr.glmnet",
     "regr.kknn",
     "regr.km",
     "regr.lm",
