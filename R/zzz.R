7f863e4cf3fc001cc55307586b0b4ce96ee2e297
Raphael Sonabend <raphael.sonabend.15@ucl.ac.uk>
2020-06-08 12:33:53 +0100

    add surv.xgboost

--- R/zzz.R
+++ R/zzz.R
@@ -44,6 +44,7 @@ register_mlr3 = function() {
   # survival learners
   x$add("surv.glmnet", LearnerSurvGlmnet)
   x$add("surv.cv_glmnet", LearnerSurvCVGlmnet)
+  x$add("surv.xgboost", LearnerSurvXgboost)
 }
 
 .onLoad = function(libname, pkgname) { # nolint
