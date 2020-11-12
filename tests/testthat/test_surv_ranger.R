cd81da37f41ddd359fc5818752a3ae30f84ba38e
Raphael Sonabend <raphael.sonabend.15@ucl.ac.uk>
2020-11-12 11:53:44 +0000

    ranger patch

--- tests/testthat/test_surv_ranger.R
+++ tests/testthat/test_surv_ranger.R
@@ -1,11 +1,13 @@
-context("surv.ranger")
-
 test_that("autotest", {
+  set.seed(1)
   learner = mlr_learners$get("surv.ranger")
   expect_learner(learner)
   learner$param_set$values = list(importance = "impurity")
   result = run_autotest(learner, check_replicable = FALSE)
   expect_true(result, info = result$error)
+})
+
+test_that("importance", {
   learner = mlr_learners$get("surv.ranger")
   expect_error(learner$importance(), "No model stored")
   expect_error(learner$train(tsk("rats"))$importance(), "No importance stored")
