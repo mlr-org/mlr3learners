b27f3c42790b30b39e530e39e9bb938949782381
Michel Lang <michellang@gmail.com>
2020-11-12 14:02:21 +0100

    reset seed in tests

--- tests/testthat/test_surv_ranger.R
+++ tests/testthat/test_surv_ranger.R
@@ -1,10 +1,11 @@
 test_that("autotest", {
-  set.seed(1)
-  learner = mlr_learners$get("surv.ranger")
-  expect_learner(learner)
-  learner$param_set$values = list(importance = "impurity")
-  result = run_autotest(learner, check_replicable = FALSE)
-  expect_true(result, info = result$error)
+  with_seed(1, {
+    learner = mlr_learners$get("surv.ranger")
+    expect_learner(learner)
+    learner$param_set$values = list(importance = "impurity")
+    result = run_autotest(learner, check_replicable = FALSE)
+    expect_true(result, info = result$error)
+  })
 })
 
 test_that("importance", {
