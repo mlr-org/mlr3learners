8c8442993b0acea60f17cd4967361ee556b72429
Raphael Sonabend <raphael.sonabend.15@ucl.ac.uk>
2021-02-01 10:16:16 +0000

    Remove predict.all from ranger (#172)

--- inst/paramtest/test_paramtest_regr.ranger.R
+++ inst/paramtest/test_paramtest_regr.ranger.R
@@ -30,6 +30,7 @@ test_that("predict regr.ranger", {
   exclude = c(
     "quantiles", # required type not supported in mlr3
     "what", # required type (quantiles) not supported in mlr3
+    "predict.all", # not supported in mlr3
     "formula", # handled via mlr3
     "object", # handled via mlr3
     "data", # handled via mlr3
