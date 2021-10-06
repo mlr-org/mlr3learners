77e1cc5fd44c7e3424d51c04773c12ce6c2b323b
Michel Lang <michellang@gmail.com>
2021-10-06 09:27:58 +0200

    add test

--- tests/testthat/test_regr_svm.R
+++ tests/testthat/test_regr_svm.R
@@ -6,3 +6,11 @@ test_that("autotest", {
   result = run_autotest(learner)
   expect_true(result, info = result$error)
 })
+
+
+test_that("autotest with type nu-regression (#209)", {
+  learner = mlr3::lrn("regr.svm", type = "nu-regression")
+  expect_learner(learner)
+  result = run_autotest(learner)
+  expect_true(result, info = result$error)
+})
