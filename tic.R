faf9d84bedb3cc8c40684c0f13ccb94dd3ba8ad5
pat-s <patrick.schratz@gmail.com>
2021-10-08 22:42:31 +0300

    fix tic.R

--- tic.R
+++ tic.R
@@ -13,8 +13,8 @@ if (!ci_has_env("PARAMTEST")) {
   get_stage("script") %>%
     # source helper_autotest.R from mlr3o
     add_step(step_install_github("mlr-org/mlr3")) %>% # remove when mlr3 0.13 is on CRAN
-    add_step(step_install_github("rvest")) %>% # for scraping xgboost params
-    add_step(step_install_github("magrittr")) %>% # for scraping xgboost params
+    add_step(step_install_cran("rvest")) %>% # for scraping xgboost params
+    add_step(step_install_cran("magrittr")) %>% # for scraping xgboost params
     add_code_step(devtools::install()) %>%
     add_code_step(testthat::test_dir("inst/paramtest",
       stop_on_failure = TRUE))
