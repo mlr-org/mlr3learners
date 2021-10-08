e3ee66787111dc5176e5bf1febd52c5bf906754d
pat-s <patrick.schratz@gmail.com>
2021-10-08 16:56:09 +0300

    fix paramtest qda

--- inst/paramtest/test_paramtest_classif.qda.R
+++ inst/paramtest/test_paramtest_classif.qda.R
@@ -2,9 +2,11 @@ library(mlr3learners)
 
 test_that("classif.qda", {
   learner = lrn("classif.qda")
-  fun = MASS::qda
+  fun = list(MASS::qda, MASS:::qda.default)
   exclude = c(
-    "x" # handled via mlr3
+    "x", # handled via mlr3
+    "grouping", # handled via mlr3
+    "CV" # handled via mlr3
   )
 
   ParamTest = run_paramtest(learner, fun, exclude, tag = "train")
@@ -17,13 +19,15 @@ test_that("classif.qda", {
 })
 
 test_that("predict classif.qda", {
-  learner = lrn("classif.lda")
+  learner = lrn("classif.qda")
   fun = MASS:::predict.qda
   exclude = c(
     "object", # handled via mlr3
     "newdata", # handled via mlr3
-    "method", # handled via mlr3 arg predict.method
-    "prior" # renamed to predict.prior, see help page
+    "method", # renamed to predict.method, see help page
+    "predict.method", # renamed from method, see help page
+    "prior", # renamed from predict.prior, see help page
+    "predict.prior" # renamed from prior, see help page
   )
 
   ParamTest = run_paramtest(learner, fun, exclude, tag = "predict")
