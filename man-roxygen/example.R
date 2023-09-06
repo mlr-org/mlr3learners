51960d3b8168c715d6a9f21e498bedd4d08bf700
be-marc <marcbecker@posteo.de>
2023-09-06 14:17:42 +0200

    refactor: change example template

--- man-roxygen/example.R
+++ man-roxygen/example.R
@@ -2,11 +2,26 @@
 lrn = mlr3::lrn(id)
 pkgs = setdiff(lrn$packages, c("mlr3", "mlr3learners"))
 %>
+#' <% task_id = if ("LearnerClassif" %in% class(lrn(id))) "spam" else "mtcars" %>
+#'
 #' @examples
 #' if (<%= paste0("requireNamespace(\"", pkgs, "\", quietly = TRUE)", collapse = " && ") %>) {
-#'   learner = mlr3::lrn("<%= id %>")
-#'   print(learner)
+#' # Define the Learner and set parameter values
+#' <%= sprintf("learner = lrn(\"%s\")", id)%>
+#' print(learner)
 #'
-#'   # available parameters:
-#' learner$param_set$ids()
+#' # Define a Task
+#' <%= sprintf("task = tsk(\"%s\")", task_id)%>
+#'
+#' # Create train and test set
+#' <%= sprintf("ids = partition(task)")%>
+#'
+#' # Train the learner on the training ids
+#' <%= sprintf("learner$train(task, row_ids = ids$train)")%>
+#'
+#' # Make predictions for the test rows
+#' <%= sprintf("predictions = learner$predict(task, row_ids = ids$test)")%>
+#'
+#' # Score the predictions
+#' predictions$score()
 #' }
