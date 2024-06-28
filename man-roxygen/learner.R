65a964056cf8046a4379beac8ac6f4721279cf0c
be-marc <marcbecker@posteo.de>
2024-06-28 13:54:45 +0200

    docs: links

--- man-roxygen/learner.R
+++ man-roxygen/learner.R
@@ -1,5 +1,5 @@
 #' @section Dictionary:
-#' This [mlr3::Learner] can be instantiated via the [dictionary][mlr3misc::Dictionary] [mlr_learners] or with the associated sugar function [mlr3::lrn()]:
+#' This [mlr3::Learner] can be instantiated via the [dictionary][mlr3misc::Dictionary] [mlr3::mlr_learners] or with the associated sugar function [mlr3::lrn()]:
 #' ```
 #' mlr_learners$get("<%= id %>")
 #' lrn("<%= id %>")
