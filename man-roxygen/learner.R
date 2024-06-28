4d8783131344d7bb9615a1e0f5480d516256ba7f
Marc Becker <33069354+be-marc@users.noreply.github.com>
2024-06-28 16:56:02 +0200

    release: 0.7.0 (#298)
    * release: 0.7.0
    
    * chore: remove remotes
    
    * chore: change maintainer
    
    * docs: maintainer
    
    * docs: fix links
    
    * docs: links
    
    * fix: links

--- man-roxygen/learner.R
+++ man-roxygen/learner.R
@@ -1,5 +1,5 @@
 #' @section Dictionary:
-#' This [Learner] can be instantiated via the [dictionary][mlr3misc::Dictionary] [mlr_learners] or with the associated sugar function [lrn()]:
+#' This [mlr3::Learner] can be instantiated via the [dictionary][mlr3misc::Dictionary] [mlr3::mlr_learners] or with the associated sugar function [mlr3::lrn()]:
 #' ```
 #' mlr_learners$get("<%= id %>")
 #' lrn("<%= id %>")
