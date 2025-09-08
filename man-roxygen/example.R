f6b81f1b544f861d7c243f68fbda5d672db9fc00
Maximilian Mücke <muecke.maximilian@gmail.com>
2025-09-08 15:34:55 +0200

    docs: update example doc template (#360)

--- man-roxygen/example.R
+++ man-roxygen/example.R
@@ -1,33 +1,29 @@
 <%
-lrn = mlr3::lrn(id)
-pkgs = setdiff(lrn$packages, c("mlr3", "mlr3learners"))
+learner = mlr3::lrn(id)
+task_id = if ("LearnerClassif" %in% class(learner)) "sonar" else "mtcars"
 %>
-#' <% task_id = if ("LearnerClassif" %in% class(lrn(id))) "sonar" else "mtcars" %>
-#'
-#' @examples
-#' if (<%= paste0("requireNamespace(\"", pkgs, "\", quietly = TRUE)", collapse = " && ") %>) {
+#' <%= sprintf("@examplesIf mlr3misc::require_namespaces(lrn(\"%s\")$packages, quietly = TRUE)", id) %>
 #' # Define the Learner and set parameter values
-#' <%= sprintf("learner = lrn(\"%s\")", id)%>
+#' <%= sprintf("learner = lrn(\"%s\")", id) %>
 #' print(learner)
 #'
 #' # Define a Task
-#' <%= sprintf("task = tsk(\"%s\")", task_id)%>
+#' <%= sprintf("task = tsk(\"%s\")", task_id) %>
 #'
 #' # Create train and test set
-#' <%= sprintf("ids = partition(task)")%>
+#' ids = partition(task)
 #'
 #' # Train the learner on the training ids
-#' <%= sprintf("learner$train(task, row_ids = ids$train)")%>
+#' learner$train(task, row_ids = ids$train)
 #'
-#' # print the model
+#' # Print the model
 #' print(learner$model)
 #'
-#' # importance method
-#' if("importance" %in% learner$properties) print(learner$importance)
+#' # Importance method
+#' if ("importance" %in% learner$properties) print(learner$importance)
 #'
 #' # Make predictions for the test rows
-#' <%= sprintf("predictions = learner$predict(task, row_ids = ids$test)")%>
+#' predictions = learner$predict(task, row_ids = ids$test)
 #'
 #' # Score the predictions
 #' predictions$score()
-#' }
