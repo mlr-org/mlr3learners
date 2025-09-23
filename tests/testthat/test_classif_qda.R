fc87ee677e47086fd8e61eab0a0fba1f083314f5
be-marc <marcbecker@posteo.de>
2025-09-23 08:02:25 +0200

    tests: increase number of observartions for lda

--- tests/testthat/test_classif_qda.R
+++ tests/testthat/test_classif_qda.R
@@ -3,6 +3,7 @@ skip_if_not_installed("MASS")
 test_that("autotest", {
   learner = mlr3::lrn("classif.qda")
   expect_learner(learner)
-  result = run_autotest(learner, N = 50L, exclude = "feat_single")
+  result = run_autotest(learner, N = 100L, exclude = "feat_single")
   expect_true(result, info = result$error)
 })
+
