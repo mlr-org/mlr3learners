cb92d915d53eb9731b40783311ca470cabd5df33
john <bblodfon@gmail.com>
2025-12-17 17:45:03 +0200

    add helper function to get base_margin for classif.xgboost

--- R/helpers_xgboost.R
+++ R/helpers_xgboost.R
@@ -49,3 +49,46 @@ xgboost_multiclass_response = function(pred, dtrain, measure, n_classes, ...) {
   response = factor(max.col(pred, ties.method = "random") - 1, levels = levels(truth))
   measure$fun(truth, response)
 }
+
+# convenience function for classif.xgboost
+# returns base_margin (vector / matrix) or NULL
+xgboost_get_base_margin = function(task, phase = "train", pv) {
+  assert_choice(phase, c("train", "predict"))
+
+  # task needs to support offset
+  if ("offset" %nin% task$properties) {
+    return(NULL)
+  }
+
+  is_predict = (phase == "predict")
+  use_pred_offset = isTRUE(pv$use_pred_offset)
+
+  # prediction phase but offset disabled
+  if (is_predict && !use_pred_offset) {
+    return(NULL)
+  }
+
+  offset = task$offset
+  n_levels = length(task$class_names)
+
+  # binary classification: return vector
+  if (n_levels == 2L) {
+    return(offset$offset)
+  }
+
+  # multiclass: return (n_obs x n_classes) matrix
+  # xgboost expects columns ordered by internal label encoding
+  # it seems reasonable to reorder according to label (0,1,2,...)
+  # which is reverted in the train function due to integer conversion
+  reordered_cols = paste0("offset_", rev(levels(task$truth())))
+  n_offsets = ncol(offset) - 1L  # excluding row_id
+
+  if (length(reordered_cols) != n_offsets) {
+    stopf(
+      "Task has %i class labels, but %i offset columns are provided",
+      n_levels, n_offsets
+    )
+  }
+
+  as_numeric_matrix(offset)[, reordered_cols, drop = FALSE]
+}
