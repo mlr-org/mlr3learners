90a6a781f0c59712e824ee04cb7e1f14699410a5
john <bblodfon@gmail.com>
2026-06-08 13:07:35 +0200

    add tests for relaxed = TRUE (classif.cv_glmnet)

--- tests/testthat/test_classif_cv_glmnet.R
+++ tests/testthat/test_classif_cv_glmnet.R
@@ -46,7 +46,6 @@ test_that("same label ordering as in glm() / log_reg", {
   }
 })
 
-
 test_that("selected_features", {
   task = tsk("iris")
   learner = lrn("classif.cv_glmnet")
@@ -61,4 +60,41 @@ test_that("selected_features", {
     learner$selected_features(Inf),
     character()
   )
+})
+
+test_that("relax = TRUE works", {
+  task = tsk("iris")
+  part = partition(task)
+  train_rows = part$train
+  test_rows = part$test
+
+  # by default, fits for gamma in (0, 0.25, 0.5, 0.75, 1)
+  learner = lrn("classif.cv_glmnet", relax = TRUE, predict_type = "prob")
+  learner$train(task, train_rows)
+  assert_class(learner$model, "cv.relaxed")
+  expect_equal(learner$model$relaxed$gamma, c(0, 0.25, 0.5, 0.75, 1))
+  # fit custom gamma values
+  gammas = seq(0, 1, length.out = 8)
+  learner$param_set$set_values(gamma = gammas)
+  learner$train(task, train_rows)
+  expect_equal(learner$model$relaxed$gamma, gammas)
+
+  p1 = learner$predict(task, test_rows)
+  # default used gamma for prediction, should not change anything
+  learner$param_set$set_values(predict.gamma = "gamma.1se")
+  p2 = learner$predict(task, test_rows)
+  expect_equal(p1$response, p2$response)
+  expect_equal(p1$prob[, "setosa"], p2$prob[, "setosa"])
+
+  # change gamma, should change predictions
+  if (learner$model$relaxed$gamma.min != learner$model$relaxed$gamma.1se) {
+    learner$param_set$set_values(predict.gamma = "gamma.min")
+    p3 = learner$predict(task, test_rows)
+    expect_false(all(p1$prob[, "setosa"] == p3$prob[, "setosa"]))
+  }
+
+  # numeric gamma value should also work and give different predictions
+  learner$param_set$set_values(predict.gamma = 0.33)
+  p4 = learner$predict(task, test_rows)
+  expect_false(all(p1$prob[, "setosa"] == p4$prob[, "setosa"]))
 })
