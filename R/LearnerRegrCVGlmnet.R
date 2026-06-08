6902184d7ed77b06fe9c05c898902a73d8cb88ac
john <bblodfon@gmail.com>
2026-06-08 23:53:57 +0200

    update/refactor regr.cv_glmnet parameters

--- R/LearnerRegrCVGlmnet.R
+++ R/LearnerRegrCVGlmnet.R
@@ -6,7 +6,8 @@
 #' Generalized linear models with elastic net regularization.
 #' Calls [glmnet::cv.glmnet()] from package \CRANpkg{glmnet}.
 #'
-#' The default for hyperparameter `family` is set to `"gaussian"`.
+#' Supported `family` values are `"gaussian"` and `"poisson"`.
+#' The default for the hyperparameter `family` is `"gaussian"`.
 #'
 #' @inheritSection mlr_learners_classif.cv_glmnet Offset
 #'
@@ -30,49 +31,56 @@ LearnerRegrCVGlmnet = R6Class(
       # fmt: skip
       # nolint start
       ps = ps(
-        alignment            = p_fct(c("lambda", "fraction"), default = "lambda", tags = "train"),
-        alpha                = p_dbl(0, 1, default = 1, tags = "train"),
-        big                  = p_dbl(default = 9.9e35, tags = "train"),
-        devmax               = p_dbl(0, 1, default = 0.999, tags = "train"),
-        dfmax                = p_int(0L, tags = "train"),
-        eps                  = p_dbl(0, 1, default = 1.0e-6, tags = "train"),
-        epsnr                = p_dbl(0, 1, default = 1.0e-8, tags = "train"),
-        exclude              = p_int(1L, tags = "train"),
-        exmx                 = p_dbl(default = 250.0, tags = "train"),
-        family               = p_fct(c("gaussian", "poisson"), default = "gaussian", tags = "train"),
-        fdev                 = p_dbl(0, 1, default = 1.0e-5, tags = "train"),
-        foldid               = p_uty(default = NULL, tags = "train"),
-        gamma                = p_uty(tags = "train", depends = quote(relax == TRUE)),
-        grouped              = p_lgl(default = TRUE, tags = "train"),
-        intercept            = p_lgl(default = TRUE, tags = "train"),
-        keep                 = p_lgl(default = FALSE, tags = "train"),
-        lambda               = p_uty(tags = "train"),
-        lambda.min.ratio     = p_dbl(0, 1, tags = "train"),
-        lower.limits         = p_uty(tags = "train"),
-        maxit                = p_int(1L, default = 100000L, tags = "train"),
-        mnlam                = p_int(1L, default = 5L, tags = "train"),
-        mxit                 = p_int(1L, default = 100L, tags = "train"),
-        mxitnr               = p_int(1L, default = 25L, tags = "train"),
-        nfolds               = p_int(3L, default = 10L, tags = "train"),
-        nlambda              = p_int(1L, default = 100L, tags = "train"),
-        use_pred_offset      = p_lgl(init = TRUE, tags = "predict"),
-        parallel             = p_lgl(default = FALSE, tags = "train"),
-        penalty.factor       = p_uty(tags = "train"),
-        pmax                 = p_int(0L, tags = "train"),
-        pmin                 = p_dbl(0, 1, default = 1.0e-9, tags = "train"),
-        prec                 = p_dbl(default = 1e-10, tags = "train"),
-        predict.gamma        = p_dbl(default = "gamma.1se", special_vals = list("gamma.1se", "gamma.min"), tags = "predict"),
-        relax                = p_lgl(default = FALSE, tags = "train"),
-        s                    = p_dbl(0, special_vals = list("lambda.1se", "lambda.min"), default = "lambda.1se", tags = "predict"),
-        standardize          = p_lgl(default = TRUE, tags = "train"),
-        standardize.response = p_lgl(default = FALSE, tags = "train"),
-        thresh               = p_dbl(0, default = 1e-07, tags = "train"),
-        trace.it             = p_int(0, 1, default = 0, tags = "train"),
-        type.gaussian        = p_fct(c("covariance", "naive"), tags = "train", depends = quote(family == "gaussian")),
-        type.logistic        = p_fct(c("Newton", "modified.Newton"), tags = "train"),
-        type.measure         = p_fct(c("deviance", "class", "auc", "mse", "mae"), default = "deviance", tags = "train"),
-        type.multinomial     = p_fct(c("ungrouped", "grouped"), tags = "train"),
-        upper.limits         = p_uty(tags = "train")
+        # glmnet::cv.glmnet() parameters
+        lambda           = p_uty(default = NULL, tags = "train"),
+        type.measure     = p_fct(c("deviance", "mse", "mae"), default = "deviance", tags = "train"),
+        nfolds           = p_int(3L, default = 10L, tags = "train"),
+        foldid           = p_uty(default = NULL, tags = "train"),
+        alignment        = p_fct(c("lambda", "fraction"), default = "lambda", tags = "train"),
+        grouped          = p_lgl(default = TRUE, tags = "train"),
+        keep             = p_lgl(default = FALSE, tags = "train"),
+        parallel         = p_lgl(default = FALSE, tags = "train"),
+        gamma            = p_uty(default = c(0, 0.25, 0.5, 0.75, 1), tags = "train", depends = quote(relax == TRUE)),
+        relax            = p_lgl(default = FALSE, tags = "train"),
+        trace.it         = p_int(0, 1, default = 0, tags = "train"), # alias: itrace
+        # glmnet::glmnet() parameters
+        family           = p_fct(c("gaussian", "poisson"), default = "gaussian", tags = "train"),
+        alpha            = p_dbl(0, 1, default = 1, tags = "train"),
+        nlambda          = p_int(1L, default = 100L, tags = "train"),
+        lambda.min.ratio = p_dbl(0, 1, tags = "train"),
+        standardize      = p_lgl(default = TRUE, tags = "train"),
+        intercept        = p_lgl(default = TRUE, tags = "train"),
+        exclude          = p_uty(default = NULL, tags = "train"),
+        penalty.factor   = p_uty(tags = "train"),
+        lower.limits     = p_uty(default = -Inf, tags = "train"),
+        upper.limits     = p_uty(default = Inf, tags = "train"),
+        type.gaussian    = p_fct(c("covariance", "naive"), tags = "train", depends = quote(family == "gaussian")),
+        # glmnet::relax.glmnet() parameters
+        maxp             = p_int(1L, tags = "train"),
+        path             = p_lgl(default = FALSE, tags = "train"),
+        # glmnet::glmnet.control() parameters
+        fdev             = p_dbl(0, 1, default = 1.0e-5, tags = "train"),
+        devmax           = p_dbl(0, 1, default = 0.999, tags = "train"),
+        eps              = p_dbl(0, 1, default = 1.0e-6, tags = "train"),
+        big              = p_dbl(default = 9.9e+35, tags = "train"),
+        mnlam            = p_int(default = 5L, tags = "train"),
+        pmin             = p_dbl(0, 1, default = 1.0e-9, tags = "train"),
+        exmx             = p_dbl(default = 250L, tags = "train"),
+        prec             = p_dbl(default = 1e-10, tags = "train"),
+        mxit             = p_int(1L, default = 100L, tags = "train"),
+        epsnr            = p_dbl(0, 1, default = 1.0e-6, tags = "train"),
+        mxitnr           = p_int(1L, default = 25L, tags = "train"),
+        thresh           = p_dbl(0L, default = 1e-07, tags = "train"),
+        maxit            = p_int(1L, default = 1e+05, tags = "train"),
+        dfmax            = p_int(default = NULL, special_vals = list(NULL), tags = "train"),
+        pmax             = p_int(default = NULL, special_vals = list(NULL), tags = "train"),
+        # glmnet::predict.cv.glmnet() and glmnet::predict.cv.relaxed() parameters
+        s                = p_dbl(0L, special_vals = list("lambda.1se", "lambda.min"), default = "lambda.1se", tags = "predict"),
+        predict.gamma    = p_dbl(0, 1, default = "gamma.1se", special_vals = list("gamma.1se", "gamma.min"), tags = "predict"), # renamed from 'gamma' to avoid duplication
+        # glmnet::predict.glmnet() parameters
+        exact            = p_lgl(default = FALSE, tags = "predict"),
+        # for using the offset during prediction
+        use_pred_offset  = p_lgl(init = TRUE, tags = "predict")
       )
       # nolint end
 
